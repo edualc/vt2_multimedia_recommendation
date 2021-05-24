844c3cc6f6912fa60f8eda201f0b940930d4bfb0
Claude Lehmann <e.dualc@hotmail.com>
2021-05-25 00:36:34 +0200

    Adjusted H5 Generation for Dataset Generator

--- util/dataset_generator.py
+++ util/dataset_generator.py
@@ -17,19 +17,19 @@ and called "keyframe_dataset.h5"
 
 EXPECTED_IMAGE_FORMAT = '.png'
 EXPECTED_IMAGE_SHAPE = (224, 224, 3)
+H5_CHUNK_SIZE = 64
 
-# lehl@2021-05-20: TODO: Uncomment here
-# 
 DATASET_PATH = config('KEYFRAME_DATASET_GENERATOR_PATH')
 
 H5_IMAGE_FEATURES = DATASET_PATH + 'image_features.h5'
 H5_LABELS = DATASET_PATH + 'image_labels.h5'
+H5_DATASET = DATASET_PATH + 'dataset.h5'
 
 def ensure_h5_dataset_exists(f, key, data):
     string_key = str(key)
 
     if string_key not in f.keys():
-        f.create_dataset(string_key, data=data)
+        f.create_dataset(string_key, data=data, compression="lzf")
     else:
         f[string_key][:] = data
 
@@ -40,82 +40,118 @@ def data_dataframe():
 
     return df3[['title','movielens_id','youtube_id','mean_rating','genres']]
 
+# f = h5py.File('MyDataset.h5', 'a')
+# for i in range(10):
+
+#   # Data to be appended
+#   new_data = np.ones(shape=(100,64,64)) * i
+#   new_label = np.ones(shape=(100,1)) * (i+1)
+
+#   if i == 0:
+#     # Create the dataset at first
+#     f.create_dataset('data', data=new_data, compression="gzip", chunks=True, maxshape=(None,64,64))
+#     f.create_dataset('label', data=new_label, compression="gzip", chunks=True, maxshape=(None,1)) 
+#   else:
+#     # Append new data to it
+#     f['data'].resize((f['data'].shape[0] + new_data.shape[0]), axis=0)
+#     f['data'][-new_data.shape[0]:] = new_data
+
+#     f['label'].resize((f['label'].shape[0] + new_label.shape[0]), axis=0)
+#     f['label'][-new_label.shape[0]:] = new_label
+
 df = data_dataframe()
-unique_genres = np.unique(np.array([item for sublist in [genre_list.split('|') for genre_list in list(df.genres.unique())] for item in sublist]))
+unique_genres = np.sort(np.unique(np.array([item for sublist in [genre_list.split('|') for genre_list in list(df.genres.unique())] for item in sublist])))
 unique_movielens_ids = np.sort(df.movielens_id.unique())
 
-with h5py.File(H5_IMAGE_FEATURES, 'a') as image_f:
-    with h5py.File(H5_LABELS, 'a') as label_f:
+with h5py.File(H5_DATASET, 'a') as dataset_f:
+    # Contains the individual Movielens-IDs for each trailer
+    # Ex.: ['123', '2394', '11023', ...]
+    # Shape:    (num_trailers,)
+    # 
+    if 'reverse_index' not in dataset_f.keys():
+        dataset_f.create_dataset('reverse_index', data=unique_movielens_ids.astype(np.int32), compression='gzip', maxshape=(None,))
+    
+    # A list of all genres to reverse the onehot encoded genres entries
+    # Ex.: ['Action', 'Sci-Fi', ...]
+    # Shape:    (num_genres,)
+    # 
+    if 'all_genres' not in dataset_f.keys():
+        dataset_f.create_dataset('all_genres', data=unique_genres.astype(h5py.string_dtype()), compression='gzip', maxshape=(None,))
+    
+    # Contains the mean ratings for each trailer
+    # Ex.: [4.7, 2.3, 3.1, ...]
+    # Shape:    (num_trailers,)
+    # 
+    if 'mean_rating' not in dataset_f.keys():
+        mean_ratings_with_id = df[['movielens_id', 'mean_rating']].to_numpy()
+        mean_ratings_with_id = mean_ratings_with_id[np.where(mean_ratings_with_id[:,0] == unique_movielens_ids)]
+        mean_ratings = np.array(np.around(mean_ratings_with_id[:,1], decimals=3)).astype(np.float16)
 
-        if 'mean_rating' not in label_f.keys():
-            label_f.create_group('mean_rating')
+        dataset_f.create_dataset('mean_rating', data=mean_ratings, compression='gzip', maxshape=(None,))
 
-        if 'genres_onehot' not in label_f.keys():
-            label_f.create_group('genres_onehot')
-
-        if 'trailer_class_onehot' not in label_f.keys():
-            label_f.create_group('trailer_class_onehot')
-
-        ensure_h5_dataset_exists(label_f, 'unique_genres', unique_genres.astype(h5py.string_dtype()))
-        ensure_h5_dataset_exists(label_f, 'unique_movielens_ids', unique_movielens_ids)
-
-        # Evaluate which files in the folder are directories
-        # that need to be iterated over
+    if 'onehot_genres' not in dataset_f.keys():
+        # Contains the onehot-encoded Genre information for each trailer
+        # Ex.: [[0,1,0,0,0,1], [1,0,0,0,0,0], ...]
+        # Shape:    (num_trailers, num_genres)
         # 
-        folders_to_process = os.listdir(DATASET_PATH)
-        folders_to_process = [folder for folder in folders_to_process if os.path.isdir(os.path.join(DATASET_PATH, folder))]
-        pbar = tqdm(folders_to_process, desc='Iterate over image folders')
+        onehot_genres = np.array([np.array([1 if genre in row else 0 for genre in unique_genres]) for row in np.char.split(df.genres.to_numpy().astype(str), sep='|')]).astype(np.byte)
 
-        for movielens_id_folder in pbar:
-            pbar.set_postfix({'movielens_id': movielens_id_folder})
-            current_movielens_id = int(movielens_id_folder)
+        dataset_f.create_dataset('onehot_genres', data=onehot_genres, compression='gzip', maxshape=(None, None))
 
-            # lehl@2021-05-20: Ensure that only those movies are processed, where
-            # meta data is available (e.g. genres) in the metadata dataframe
-            # 
-            if current_movielens_id not in unique_movielens_ids:
-                continue
+    # ===================================================================
 
-            # Check which files are images
-            # 
-            files = os.listdir(os.path.join(DATASET_PATH, movielens_id_folder))
-            images = [image for image in files if EXPECTED_IMAGE_FORMAT in image]
-            del files
+    # Evaluate which files in the folder are directories
+    # that need to be iterated over
+    # 
+    folders_to_process = os.listdir(DATASET_PATH)
+    folders_to_process = [folder for folder in folders_to_process if os.path.isdir(os.path.join(DATASET_PATH, folder))]
+    pbar = tqdm(folders_to_process, desc='Iterate over image folders')
 
-            # If the h5file is missing this movielens key, add the dummy array
-            # and generate the dataset inside
-            #
-            ensure_h5_dataset_exists(image_f, current_movielens_id, np.zeros((len(images),) + EXPECTED_IMAGE_SHAPE))
+    for movielens_id_folder in pbar:
+        pbar.set_postfix({'movielens_id': movielens_id_folder})
+        current_movielens_id = int(movielens_id_folder)
 
-            # Fill the images into the h5 file
-            # 
-            for i, image in enumerate(tqdm(images, leave=False)):
-                image_f[str(current_movielens_id)][i] = io.imread(os.path.join(DATASET_PATH, movielens_id_folder, image))
+        # lehl@2021-05-20: Ensure that only those movies are processed, where
+        # meta data is available (e.g. genres) in the metadata dataframe
+        # 
+        if current_movielens_id not in unique_movielens_ids:
+            continue
+
+        # Check which files are images
+        # 
+        files = os.listdir(os.path.join(DATASET_PATH, movielens_id_folder))
+        images = [image for image in files if EXPECTED_IMAGE_FORMAT in image]
+        del files
+
+        tmp = np.zeros((len(images),) + EXPECTED_IMAGE_SHAPE)
+        tmp_indices = np.zeros((len(images), 2), dtype=np.int32)
+
+        reverse_index = np.where(current_movielens_id == dataset_f['reverse_index'][:])[0][0]
+
+        # Fill the images into the h5 file
+        # 
+        for i, image in enumerate(tqdm(images, leave=False)):
+            tmp[i,:,:,:] = io.imread(os.path.join(DATASET_PATH, movielens_id_folder, image))
+            tmp_indices[i] = np.array([reverse_index, i])
+
+        # Contains the image data (main part of the features) for each keyframe
+        # Ex.: [[<224,224,3> Image], [<224,224,3> Image], ...]
+        # Shape:    (num_trailers, (image_dimensions, such as 224,224,3))
+        # 
+        if 'keyframes' not in dataset_f.keys():
+            dataset_f.create_dataset('keyframes', data=tmp, compression='gzip', maxshape=(None,) + EXPECTED_IMAGE_SHAPE, chunks=(H5_CHUNK_SIZE,) + EXPECTED_IMAGE_SHAPE)
+    
+        else:
+            dataset_f['keyframes'].resize((dataset_f['keyframes'].shape[0] + tmp.shape[0]), axis=0)
+            dataset_f['keyframes'][-tmp.shape[0]:, :, :, :] = tmp
+
+        # Contains the indices to find the (Trailer, Keyframe) Tuple for each keyframe
+        # Ex.: [[0,0], [17,3], [3,88]] --> [trailer_index in reverse_index, keyframe_index]
+        # Shape:    (num_keyframes, 2)
+        # 
+        if 'indices' not in dataset_f.keys():
+            dataset_f.create_dataset('indices', data=tmp_indices, compression='gzip', maxshape=(None, 2), chunks=(H5_CHUNK_SIZE,2))
             
-            # Build up the relevant label information
-            # 
-            movie_metadata = df[df.movielens_id==current_movielens_id]
-
-            trailer_labels = {
-                'mean_rating': movie_metadata.mean_rating.to_numpy(),
-                'genres': movie_metadata.genres.to_numpy()[0].split('|'),
-                'trailer_class_onehot': np.zeros(unique_movielens_ids.size)
-            }
-
-            trailer_labels['trailer_class_onehot'][np.where(unique_movielens_ids==current_movielens_id)[0][0]] = 1.0
-            trailer_labels['genres_onehot'] = np.array([1.0 if genre in trailer_labels['genres'] else 0.0 for genre in unique_genres])
-            del trailer_labels['genres']
-
-            # lehl@2021-05-21: Is it necessary, that these labels are tiled (np.tile)? 
-            # In practice, the labels are the same for all images/frames of a trailer and
-            # could be "non-duplicated".
-            # 
-            ensure_h5_dataset_exists(label_f['mean_rating'], current_movielens_id, np.tile(trailer_labels['mean_rating'], (len(images),1)))
-            ensure_h5_dataset_exists(label_f['genres_onehot'], current_movielens_id, np.tile(trailer_labels['genres_onehot'], (len(images),1)))
-            ensure_h5_dataset_exists(label_f['trailer_class_onehot'], current_movielens_id, np.tile(trailer_labels['trailer_class_onehot'], (len(images),1)))
-
-        # import code; code.interact(local=dict(globals(), **locals()))
-
-
-
-
+        else:
+            dataset_f['indices'].resize((dataset_f['indices'].shape[0] + tmp_indices.shape[0]), axis=0)
+            dataset_f['indices'][-tmp_indices.shape[0]:, :] = tmp_indices
