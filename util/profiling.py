f34b9571173730ff57852e5d9f421818ec4255f2
Claude Lehmann <e.dualc@hotmail.com>
2021-06-26 18:21:51 +0200

    add profiling util

---
+++ util/profiling.py
@@ -0,0 +1,41 @@
+import os
+import cProfile
+import pstats
+from datetime import datetime
+
+# import asyncio
+
+# lehl@2021-06-26: Profiling function built, inspired by the 
+# mCoding video about profiling in Python:
+# --> https://www.youtube.com/watch?v=m_a0fN48Alw
+# 
+# Run "snakeviz <profiling_file>" to view a visualization
+#
+def start_profiling(func, *args, print_stats=True, save_stats=True, **kwargs):
+    pr = cProfile.Profile()
+    pr.enable()
+    func(*args, **kwargs)
+    pr.disable()
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+
+    if print_stats:
+        stats.print_stats()
+
+    if save_stats:
+        if not os.path.exists('_profiling_dumps'):
+            os.makedirs('_profiling_dumps')
+
+        time_stamp = datetime.now().strftime('%Y_%m_%d__%H%M%S')
+        file_name = '_profiling_dumps/' + '_'.join([time_stamp, func.__name__ + '.prof'])
+        stats.dump_stats(file_name)
+
+if __name__ == '__main__':
+    def do_stuff():
+        x = np.arange(2**20)
+        y = np.arange(2**22)
+        z = np.intersect1d(x,y)
+
+    import numpy as np
+    start_profiling(do_stuff)
