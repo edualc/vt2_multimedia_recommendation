938ac121d4d0af9d08dc507627cb3ee97dfa15af
Claude Lehmann <e.dualc@hotmail.com>
2021-07-08 16:03:29 +0200

    adjust resources on cluster

--- run_on_zhaw_cluster.sh
+++ run_on_zhaw_cluster.sh
@@ -1,1 +1,1 @@
-srun --pty --ntasks=1 --cpus-per-task=4 --mem=64G --gres=gpu:1 singularity shell /cluster/home/lehl/docker/vt2_autodl.simg
+srun --pty --ntasks=1 --cpus-per-task=3 --mem=48G --gres=gpu:1 singularity shell /cluster/home/lehl/docker/vt2_autodl.simg
